97a3a5eec0ec30bca544a6b9949100dafb80a701
Akshay Kumar <email.akshay98@gmail.com>
2020-01-28 18:32:32 +0530

    add image gradient

--- client/src/components/DisplayMarkdown/DisplayMarkdown.jsx
+++ client/src/components/DisplayMarkdown/DisplayMarkdown.jsx
@@ -3,15 +3,21 @@ import ReactMarkdown from 'react-markdown'
 import { connect } from 'react-redux'
 
 const DisplayMardown = (props) => {
+    const getTimeFormat = (index) => {
+        if(index >= 12) {
+            return `${index}:00 AM`
+        } else {
+            return `${index}:00 PM`
+        }
+    }
     const generateScrum = () => {
         let markdown = "# Scrum Report"
-        
         const date = new Date();
         let time;
         for(time in props.times) {
             if(props.times[time].text) {
-                markdown += `\n ## ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}-Hour: ${time}`
-                markdown += `\n ${props.times[time].text}`
+                markdown += `\n ## ${date.getDate()}-${date.getMonth()}-${date.getFullYear()}, ${getTimeFormat(time)}`
+                markdown += `\n #### ${props.times[time].text}`
             }
         }
         markdown += "<br/> <br/>"
--- client/src/components/Hours/Hours.jsx
+++ client/src/components/Hours/Hours.jsx
@@ -3,12 +3,19 @@ import { connect } from 'react-redux'
 import './Hours.css'
 
 const Hours = (props) => {
+    const getTimeFormat = (index) => {
+        if(index >= 12) {
+            return `${index}:00 AM`
+        } else {
+            return `${index}:00 PM`
+        }
+    }
     const getColor = (index) => {
         var current = new Date();
-        return current.getHours() === index ? 'cornflowerblue' : current.getHours() > index ? 'orangered' : 'grey';
+        return current.getHours() === index ? '#8BC34A' : current.getHours() > index ? '#f44336' : '#78909C';
     }
     const getButtons = () => {
-        return props.times.map((value, index) => <button className="HourButton" key={index} style={{backgroundColor: getColor(index)}}>{`Hour: ${index}`}</button>)
+    return props.times.map((value, index) => <button className="HourButton" key={index} style={{backgroundColor: getColor(index)}}>{getTimeFormat(index)}</button>)
     }
     return getButtons()   
 }
